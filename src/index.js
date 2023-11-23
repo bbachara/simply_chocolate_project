ff43834c646e11164a1b798c149709a4ebc1e9e9
KamaWalk <nierudylis@gmail.com>
2023-11-23 09:30:45 +0100

    modale dwa z trzech działają

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './our-product.js';
 import './mobile-menu.js';
 import './sass/main.scss';
 import './script.js';
-import './modal.js';
-
+import './modal-subscribe.js';
+import './modal-review.js';
